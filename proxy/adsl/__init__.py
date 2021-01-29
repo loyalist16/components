5832a5da8cc6b5b1ab622479873fd172804f1d74
loyalist16 <1024343932@qq.com>
2021-01-29 09:35:00 +0800

    Initial commit

---
+++ proxy/adsl/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/1/29 9:22
+# @Author : loyalist
+# @Email : lzl0118@foxmail.com
+# @File : __init__.py.py
+# @Desc :
---
+++ proxy/dbConnect.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/1/29 9:33
+# @Author : loyalist
+# @Email : lzl0118@foxmail.com
+# @File : dbConnect.py
+# @Desc : 链接各种数据库
+
